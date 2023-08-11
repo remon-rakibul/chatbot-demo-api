4085396ab3079fd8dd5a175c02892eaad331a6d7
remon-rakibul <rakibul.arisaftech@gmail.com>
2023-08-11 15:53:46 +0600

    feat: added logger

---
+++ logger_config.py
@@ -0,0 +1,21 @@
+import logging
+
+
+def setup_logger(name) -> logging.Logger:
+    FORMAT = '%(asctime)s - %(message)s'
+    TIME_FORMAT = '%d-%b-%y %H:%M:%S'
+
+    logging.basicConfig(
+        format=FORMAT, datefmt=TIME_FORMAT, level=logging.INFO, filename="api.log"
+    )
+
+    logger = logging.getLogger(name)
+    return logger
+
+
+# in any file that import fn setup_logger from the above 'logger_config.py', you can set up local logger like:
+# local_logger = setup_logger(__name__)
+
+# local_logger.info(
+#     f"I am writing to file {FILENAME}. If that file did not exist, it would be automatically created. Here, you can change me to write about the call to a specific route function etc."
+# )

6a8cb38ac9a8f0bef060cd99a951162d2bd6588d
HazratAli <hazrat.arisaftech@gmail.com>
2023-08-16 16:30:55 +0600

    Added file upload and download functionalities

--- api.py
+++ api.py
@@ -11,12 +11,29 @@ import tempfile
 import uvicorn
 import prompt
 import os
+from pathlib import Path
+from datetime import datetime
+from bson.objectid import ObjectId 
+from bson.json_util import dumps
+from fastapi.responses import JSONResponse, FileResponse
+from pymongo import MongoClient
+import tempfile
 
 
 class Prompt(BaseModel):
     prompt: str
 
 app = FastAPI()
+
+client = MongoClient("mongodb://localhost:27017")
+# db = client("file_storage") 
+db = client.file_storage
+file_collection = db["files"]
+
+class FileUpload(BaseModel):
+    name: str
+    content: bytes
+    time: str
 
 # origins = [
 #     "http://localhost:8000",
@@ -81,6 +98,58 @@ async def chat(query: Annotated[str, Form()]):
         return {"response": response}
 
 
+@app.post("/uploadfile")
+async def upload_file(file: UploadFile = File(...)):
+
+    current_datetime = datetime.now()
+    
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+    content = await file.read()
+    file_data = {
+        "name": file.filename,
+        "content": content,
+        "time": formatted_datetime
+    }
+
+    file_collection.insert_one(file_data)
+    # DIR = "temp"
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+
+    print(file_data["time"])
+
+
+    with open(os.path.join("temp", file_data["time"]), "wb") as f:
+        f.write(content)
+
+    return {"Message": "File upload successful"}
+
+
+@app.get("/all")
+async def get_files():
+    files = list (file_collection.find())
+    serialized_files = dumps(files)
+    return JSONResponse(content=serialized_files)
+
+@app.get("/download/{file_id}")
+async def download_file(file_id: str):
+    
+    file_data = file_collection.find_one({"_id": ObjectId(file_id)})
+
+    if not file_data:
+        return JSONResponse(content={"message": "File not found"}, status_code=404)
+    
+    file_content = file_data["content"]
+    file_name = file_data["name"] 
+    file_time = file_data["time"]
+
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+    
+    file_path = Path("temp") / file_time
+    print(file_path)
+    # return FileResponse(file_path, file_name)
+    return FileResponse(file_path, media_type='application/octet-stream',filename=file_name)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

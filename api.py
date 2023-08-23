b3e197f894bdc2a61b55564685937bb37aee60b4
remon-rakibul <rakibul.arisaftech@gmail.com>
2023-08-23 15:16:09 +0600

    fix: upload issue

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import TextLoader
 from pydantic import BaseModel
 from typing import Annotated
-# import embedding
+import embedding
 import tempfile
 import uvicorn
 import prompt
@@ -17,7 +17,7 @@ from bson.objectid import ObjectId
 from bson.json_util import dumps
 from fastapi.responses import JSONResponse, FileResponse
 from pymongo import MongoClient
-import tempfile
+# import tempfile
 
 
 class Prompt(BaseModel):
@@ -25,7 +25,7 @@ class Prompt(BaseModel):
 
 app = FastAPI()
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://5.189.160.223:27017/")
 # db = client("file_storage") 
 db = client.file_storage
 file_collection = db["files"]
@@ -58,27 +58,29 @@ async def health():
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
 
-
-    current_datetime = datetime.now()
+    # print("Here")
+    # current_datetime = datetime.now()
     
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-    content = await file.read()
-    file_data = {
-        "name": file.filename,
-        "content": content,
-        "time": formatted_datetime
-    }
+    # formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+    # content = await file.read()
+    # file_data = {
+    #     "name": file.filename,
+    #     "content": content,
+    #     "time": formatted_datetime
+    # }
 
-    file_collection.insert_one(file_data)
-    # DIR = "temp"
-    if not os.path.exists("temp"):
-        os.makedirs("temp")
+    # file_collection.insert_one(file_data)
+    # # DIR = "temp"
+    # if not os.path.exists("temp"):
+    #     os.makedirs("temp")
 
-    print(file_data["time"])
+    # print(file_data["time"])
 
 
-    with open(os.path.join("temp", file_data["time"]), "wb") as f:
-        f.write(content)
+    # with open(os.path.join("temp", file_data["time"]), "wb") as f:
+    #     f.write(content)
+
+    
 
 
     app.ast = False
@@ -88,27 +90,55 @@ async def upload(file: UploadFile = File(...)):
 
     with open(file_path, "wb") as f:
         f.write(file.file.read())
-
+    print(f"This is the filename: {file.filename}")
     if (file.filename.endswith(".pdf")):
         loader = PyPDFLoader(file_path)
         pages = loader.load_and_split()
-        # app.db = embedding.load_file_to_db(pages)
+        app.db = embedding.load_file_to_db(pages)
 
     elif (file.filename.endswith(".docx") or file.filename.endswith(".doc")):
+        print(f"this is the file_path: {file_path}")
         loader = Docx2txtLoader(file_path)
+        print("working fine....")
         data = loader.load()
-        # app.db = embedding.load_file_to_db(data)
+        app.db = embedding.load_file_to_db(data)
 
     elif (file.filename.endswith(".ppt") or file.filename.endswith(".pptx")):
         loader = UnstructuredPowerPointLoader(file_path)
         data = loader.load()
-        # app.db = embedding.load_file_to_db(data)
+        app.db = embedding.load_file_to_db(data)
 
     elif (file.filename.endswith(".txt")):
         loader = TextLoader(file_path)
         data = loader.load()
-        # app.db = embedding.load_file_to_db(data)
+        app.db = embedding.load_file_to_db(data)
 
+    try:
+        print("Here")
+        current_datetime = datetime.now()
+        
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+        content = await file.read()
+        file_data = {
+            "name": file.filename,
+            "content": content,
+            "time": formatted_datetime
+        }
+
+        file_collection.insert_one(file_data)
+        # DIR = "temp"
+        if not os.path.exists("temp"):
+            os.makedirs("temp")
+
+        print(file_data["time"])
+
+
+        with open(os.path.join("temp", file_data["time"]), "wb") as f:
+            pass
+            # f.write(content)
+    except Exception as e:
+        print(f"This is the Exception: {e}")
+    
     return {"response": f"{file.filename} uploaded successfully"}
 
 
@@ -151,9 +181,20 @@ async def upload_file(file: UploadFile = File(...)):
 
 @app.get("/all")
 async def get_files():
-    files = list (file_collection.find())
-    serialized_files = dumps(files)
-    return JSONResponse(content=serialized_files)
+    # files = list (file_collection.find())
+    # serialized_files = dumps(files)
+    # return JSONResponse(content=serialized_files)
+    files = list(file_collection.find())
+    serialized_files = []
+
+    for file in files:
+        serialized_files.append({
+            "_id": str(file["_id"]),
+            "name": file["name"],
+            "time": file["time"]
+        })
+        print("testing")
+    return serialized_files
 
 @app.get("/download/{file_id}")
 async def download_file(file_id: str):
